a4929292156249ca68c9c316d9fa0349782af266
CMHyuk <alsgur9710@naver.com>
2023-03-13 20:46:43 +0900

    qr코드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [text, setText] = useState('');
+
+  const generateQRCode = () => {
+    fetch(`/qrcode?text=${text}`)
+        .then((response) => response.arrayBuffer())
+        .then((buffer) => {
+          const base64 = btoa(new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ''));
+          const src = `data:image/png;base64,${base64}`;
+          setImageSrc(src);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={generateQRCode}>Generate QR code</button>
+        {imageSrc && <img src={imageSrc} alt="QR code" />}
+      </div>
   );
 }
 
